2bca4ca2d9765136ad62ba47c8bdb93bcebe8695
Reba507 <52818012+Reba507@users.noreply.github.com>
2025-11-07 11:47:07 +0200

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,21 +1,22 @@
--- Dimension Table
+
 CREATE TABLE IF NOT EXISTS dim_agency (
     agency VARCHAR(10) PRIMARY KEY,
     agency_name VARCHAR(100)
 );
 INSERT INTO dim_agency VALUES ('NYPD', 'New York Police Department'), ('DOT', 'Department of Transportation');
 
--- JOIN + Aggregate
+
 SELECT a.agency_name, COUNT(*) as complaints
 FROM fact_311 f
 INNER JOIN dim_agency a ON f.agency = a.agency
 GROUP BY a.agency_name
 ORDER BY complaints DESC;
 
--- CTE for Hourly Complaints
+
 WITH hourly AS (
     SELECT EXTRACT(HOUR FROM created_date) as hour, COUNT(*) as complaints
     FROM fact_311 GROUP BY hour
 )
 SELECT hour, complaints
+
 FROM hourly ORDER BY complaints DESC;
