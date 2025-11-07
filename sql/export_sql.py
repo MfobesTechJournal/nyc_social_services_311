e7b1245a310ecefffdbef9d18d734adbc8e7d7e9
Reba507 <52818012+Reba507@users.noreply.github.com>
2025-11-07 11:46:01 +0200

    Update export_sql.py
    Added a print statement to confirm the number of exported rows.

--- sql/export_sql.py
+++ sql/export_sql.py
@@ -1,4 +1,4 @@
-# sql/export_sql.py
+
 import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
@@ -9,7 +9,8 @@ load_dotenv()
 db_url = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 engine = create_engine(db_url)
 
-# Export full table
+
 df = pd.read_sql("SELECT * FROM fact_311", engine)
 df.to_csv("results/sql_export.csv", index=False)
+
 print(f"Exported {len(df)} rows to results/sql_export.csv")
